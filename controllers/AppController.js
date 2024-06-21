879a4300c4399d7787ec00bfad80d44109db793b
El imali hamza <elimal.hamza@gmail.com>
2024-06-21 06:39:07 -0700

    .

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,16 +3,15 @@ import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
-    return res
-      .status(200)
-      .json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
-  }
-  static getStats = async (req, res) => {
-    return res.status(200).json({
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
-  };
+  }
+
+  static async getStats(req, res) {
+    res.status(200).json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
+  }
 }
 
 export default AppController;
